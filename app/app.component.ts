55422ddb2f200a3d8285bd826666df8aa9b6e84f
Ethan Law <ejlaw01@gmail.com>
2017-01-26 13:33:55 -0800

    add initial Sass setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <h1>Wildbook</h1>
   `
 })
 
 export class AppComponent {
-  
+
 }
